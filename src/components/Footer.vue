a9f7f66920d71a03c9a109c79b6fbc46336d656c
macabc <anej.batagelj@gmail.com>
2022-01-10 13:57:32 +0100

    first changes

---
+++ src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="footer">
+    <p>
+      this is multi use server
+    </p>
+    <h3>Athor</h3>
+    <a>Anej Batagelj</a>
+
+    <h3>Main Links</h3>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
+      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+}
+</script>
+
+<style scoped>
+
+</style>
